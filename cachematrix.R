c27b55baf84eb84d4b7ca2049c433a2b8732b2e6
John-Phila <87459097+John-Phila@users.noreply.github.com>
2021-07-15 11:41:22 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special "matrix" labeled as 'philip'
+## The function is able to create a matrix where the inverse can be cahed
 
-## Write a short comment describing this function
+## Programming is not my league 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(philip = matrix()) {
+phila <- NULL
+set <- function(y){
+    philip <<- y
+    phila <<- NULL         
+}
+get <- function() {phila}
+setInverse <- function(solceMatrix) {phila <<- solveMatrix}
+getInverse <- function() {phila}
+list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## The following function enables the inverse of a special "matrix" to be solved.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(philip, ...) {
+        ## Return a matrix that is the inverse of 'philip'
+        phila <- x$getInverse()
+        if(!is.null(phila)){
+                message("getting cached data")
+                return(phila)
+        }
+        data <- x$get()
+        phila <- solve(met, ...)
+        x$setInverse(phila)
+        phila
 }
